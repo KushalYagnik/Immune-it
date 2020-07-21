64212a4dd7ce15b8285ae04fc4c09fe3985c62ad
KushalYagnik <kushalyagnik@gmail.com>
2020-07-21 00:43:30 -0400

    'AddRecord' modal integrated into Records page

--- immune-it/src/components/Records/Records.jsx
+++ immune-it/src/components/Records/Records.jsx
@@ -1,14 +1,56 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import './Records.scss';
+import AddRecord from '../AddRecord/AddRecord';
 
 export class Records extends Component {
+    constructor() {
+        super();
+        this.state = { show: false }
+    }
+
+    showModal = () => {
+        this.setState({ show: true });
+    };
+
+    hideModal = () => {
+        this.setState({ show: false });
+    };
+
     render() {
         return (
-            <div>
-                <h1>You're in Records page now!</h1>
-                
+            <div className="records">
+                <h1 className="records__title">Manage your immunization records</h1>
+                <Modal show={this.state.show} handleClose={this.hideModal}>
+                    <AddRecord/>
+                </Modal>
+                <ul className="records__list">
+                    <li className="records__item">
+                        <a className="records__link" href="#">Sample Record</a>
+                    </li>
+                </ul>
+                <button type="button" onClick={this.showModal}>Open</button>
             </div>
         )
     }
 }
+
+const Modal = ({ handleClose, show, children }) => {
+    const showHideClassName = show ? 'modal display-block' : 'modal display-none';
+  
+    return (
+      <div className={showHideClassName}>
+        <section className='modal-main'>
+          {children}
+          <button
+            onClick={handleClose}
+          >
+            Close
+          </button>
+        </section>
+      </div>
+    );
+  };
+
+
 
 export default Records
